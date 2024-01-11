7d1ae572ec28c1a63502bccf5773dfaa7c63d0c6
Nayan Desai <ndmaratha01@gmail.com>
2024-01-11 09:50:26 +0530

    all packages installed

---
+++ youtube/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './index.css'
+
+function App() {
+
+
+  return (
+    <div className='flex items-center justify-center'>
+    <h1 className="font-bold text-red-500 text-2xl">Shree Ganeshay namah</h1>
+    </div>
+  )
+}
+
+export default App
